301ffb9135c3f8ea845b555cb92c5a7f07bde4a8
Sathia <smuthu@archsystemsinc.com>
2021-02-27 13:29:02 +0530

    Download code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private appService: AppService) { }
+
   title = 'test-angular';
+
+  downloadExcel() {
+    this.appService.downloadFile().subscribe(res => {
+      this.downloadFile(res, 'sathia.xlsx');
+    });
+  }
+
+  downloadFile(data: Blob, fileName: any) {
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(data);
+    link.download = fileName;
+    link.click();
+  }
+
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AppService {
+
+    constructor(private httpClient: HttpClient){}
+
+    downloadFile(): Observable<any> {
+        return this.httpClient.get('http://localhost:8080/api/excel', {responseType: 'blob'})
+    }
+
+}
